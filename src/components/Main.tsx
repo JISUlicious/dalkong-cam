cde528bd50b002a6292132a4e5039aa1cf2a0110
JISUlicious <ljshong@hanmail.net>
2023-04-21 11:03:17 +0900

    create/join room button

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,28 @@
 import {Link} from "react-router-dom";
 import {useAuthContext} from "../contexts/AuthContext";
+import React from "react";
 
 export function Main () {
   const { user } = useAuthContext();
+
+  function onClick (event: React.MouseEvent<HTMLButtonElement>) {
+    const buttonValue: string = event.currentTarget.value;
+    console.log(buttonValue);
+  }
+
   return <div>
     <h1>Main</h1>
-    { user?.uid }
+    { user?.email }
     <Link to={"/room/create"}>to CreateRoom</Link>
+    <div>
+      <button onClick={onClick} value="create">
+        create room
+      </button>
+    </div>
+    <div>
+      <button onClick={onClick} value="join">
+        join room
+      </button>
+    </div>
   </div>;
 }
