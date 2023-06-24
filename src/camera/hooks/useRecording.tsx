9ecfa95a3e98150d9b0568231f87d0bc4f352f44
JISUlicious <ljshong@hanmail.net>
2023-06-24 21:02:05 +0900

    useRecording hook

--- src/camera/hooks/useRecording.tsx
+++ src/camera/hooks/useRecording.tsx
@@ -1,13 +1,36 @@
-import { Dispatch, useEffect } from "react";
+import { Dispatch, RefObject, useEffect } from "react";
+import { detectMotion, timer } from "../../common/functions/detectMotion";
+import { Action, ConnectionActionCreator } from "../../common/contexts/ConnectionContext";
 
-export function useRecording (track: MediaStreamTrack, setCaptured: Dispatch<React.SetStateAction<any>>) {
-  const timeInMs = 1000;
-  useEffect(() => {
-    const interval = setInterval(() => {
-      console.log(`Captures every ${timeInMs/1000} sec`);
+export function useRecording (
+  canvasRef: RefObject<HTMLCanvasElement>, 
+  streamRef: RefObject<HTMLVideoElement>,
+  dispatch: Dispatch<Action>,
+  ) {
+  useEffect(()=>{
+    if (canvasRef && streamRef) {
+      const canvas = canvasRef.current;
+      const context = canvasRef.current?.getContext('2d');
       
-    }, timeInMs);
-  
-    return () => clearInterval(interval);
-  }, []);
+      const captureInterval = 100;
+      const width = 64;
+      const height = 48;
+      
+      canvas!.style.display = "none";
+      canvas!.width = width;
+      canvas!.height = height;
+
+      const interval = setInterval(async ()=>{
+        context?.drawImage(streamRef.current!, 0, 0, width, height);   
+        await timer(captureInterval);
+        context?.drawImage(streamRef.current!, 0, 0, width, height);
+        
+        const isRecording = await detectMotion(context);
+        dispatch(ConnectionActionCreator.setIsRecording(isRecording));
+        
+        context?.clearRect(0, 0, width, height);
+      }, captureInterval * 2);
+      return () => clearInterval(interval);
+    }
+  }, [canvasRef, streamRef]);
 }
--- src/common/functions/detectMotion.tsx
+++ src/common/functions/detectMotion.tsx
@@ -1,79 +1,36 @@
 
-
 const PIXEL_SCORE_THRESHOLD = 3;
 const IMAGE_SCORE_THRESHOLD = 50;
 
-export function capture(
-  context: CanvasRenderingContext2D | null | undefined, 
-  video: HTMLVideoElement | null
-  ) {
-
-  if (context && video) {
-    context?.drawImage(video, 0, 0, 64, 48);    
-  }
-}
-
 export async function detectMotion (
   context: CanvasRenderingContext2D | null | undefined, 
-  // recording: boolean, 
-  // dispatch: Dispatch<Action>
   ) {
   if (context) {
-    // console.log("detecting motion");
     context.globalCompositeOperation = 'difference';
 
     const imageData = context.getImageData(0,0,64,48);
-    // console.log("Image Data",imageData);
     let imageScore = 0;
 
     for (let i = 0; i < imageData.data.length; i += 4) {
-        const r = imageData.data[i] / 3;
-        const g = imageData.data[i + 1] / 3;
-        const b = imageData.data[i + 2] / 3;
-        // const pixelScore = r + g + b;
-        const pixelScore = r;
+      const r = imageData.data[i] / 3;
+      // const g = imageData.data[i + 1] / 3;
+      // const b = imageData.data[i + 2] / 3;
+      // const pixelScore = r + g + b;
+      const pixelScore = r;
 
-        // console.log("pixel score", pixelScore);
-        if (pixelScore >= PIXEL_SCORE_THRESHOLD) {
-          imageScore++;
-        }
+      if (pixelScore >= PIXEL_SCORE_THRESHOLD) {
+        imageScore++;
+      }
     }
 
-    if (imageScore >= IMAGE_SCORE_THRESHOLD) { // && recording===false) {
-      // we have motion!
-      console.log("motion detected", new Date(), "Image Score", imageScore);
-      // dispatch(ConnectionActionCreator.setLocalStreamAttribute({}));
-      // let newDiv = await createDivForSavedVideo();
-      // savedVideos.prepend(newDiv);
-      // inMotion = false;
+    if (imageScore >= IMAGE_SCORE_THRESHOLD) {
       return true;
     } else {
-      console.log("Image Score",imageScore);
       return false;
     }
+  } else {
+    return false;
   }
-  // do other stuff
-}
-
-export async function detectMotionFromStream (
-  canvas: HTMLCanvasElement | null,
-  context: CanvasRenderingContext2D | null | undefined, 
-  video: HTMLVideoElement | null
-) {
-  // console.log("detecting motion from stream",new Date());
-
-  capture(context, video);
-  console.log("first capture")
-  await timer(5000);
-  capture(context, video);
-  const recorded = await detectMotion(context);
-
-  context?.clearRect(0, 0, canvas!.width, canvas!.height);
-  console.log("second capture")
-  await timer(5000);
-  console.log("recorded", recorded, "detectMotion stdby",new Date());
-  // console.log("===========================================");
-  return true;
 }
 
 export function timer (time: number) {
