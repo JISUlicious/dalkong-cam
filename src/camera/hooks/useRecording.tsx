ae28ccfcd762f074c8400807a67c3ea719eb33bd
JISUlicious <ljshong@hanmail.net>
2023-06-24 23:54:02 +0900

    recording

--- src/camera/hooks/useRecording.tsx
+++ src/camera/hooks/useRecording.tsx
@@ -1,10 +1,13 @@
 import { Dispatch, RefObject, useEffect } from "react";
 import { detectMotion, timer } from "../../common/functions/detectMotion";
-import { Action, ConnectionActionCreator } from "../../common/contexts/ConnectionContext";
+import { Action, ConnectionActionCreator, StreamAttributes } from "../../common/contexts/ConnectionContext";
+import { time } from "console";
 
 export function useRecording (
   canvasRef: RefObject<HTMLCanvasElement>, 
   streamRef: RefObject<HTMLVideoElement>,
+  localStreamAttributes: StreamAttributes,
+  recorder: MediaRecorder | undefined,
   dispatch: Dispatch<Action>,
   ) {
   useEffect(()=>{
@@ -25,12 +28,46 @@ export function useRecording (
         await timer(captureInterval);
         context?.drawImage(streamRef.current!, 0, 0, width, height);
         
-        const isRecording = await detectMotion(context);
-        dispatch(ConnectionActionCreator.setIsRecording(isRecording));
+        const motionDetected = await detectMotion(context);
+        
+        if (motionDetected) {
+          const motionDetectedTime = new Date();
+          dispatch(ConnectionActionCreator.setIsRecording(motionDetected, motionDetectedTime));
+        } else {
+          dispatch(ConnectionActionCreator.setIsRecording(motionDetected, null));
+        }
         
         context?.clearRect(0, 0, width, height);
       }, captureInterval);
-      return () => clearInterval(interval);
+      return () => {
+        context?.clearRect(0, 0, width, height);
+        clearInterval(interval);
+      };
     }
   }, [canvasRef, streamRef]);
+
+  useEffect(() => {
+    
+    console.log(localStreamAttributes);
+    console.log(recorder);
+
+    // TODO : handle recorded blob, push to storage
+    if (localStreamAttributes.isRecording && recorder?.state === "inactive") {
+      recorder.start();
+    } else if (
+      !localStreamAttributes.isRecording 
+      && recorder?.state === "recording"
+      && localStreamAttributes.lastMotionDetected
+      ) {
+      const date = new Date(); 
+      const timeSinceLastMotion = date.getTime() - localStreamAttributes.lastMotionDetected.getTime();
+
+      if (timeSinceLastMotion > 3000) {
+        recorder.stop();
+        console.log("stopped");
+        console.log(recorder);
+      } 
+    }
+
+  }, [localStreamAttributes]);
 }
