3ef655af686cfcc467d8a14185226ec644c51b46
JISUlicious <ljshong@hanmail.net>
2023-07-05 18:50:58 +0900

    eventlistner on duration change

--- src/common/components/VideoItem.tsx
+++ src/common/components/VideoItem.tsx
@@ -8,16 +8,27 @@ interface SavedVideoProps {
   duration?: number
 }
 
+
 export function VideoItem ({title, url, duration}: SavedVideoProps) {
   const [initialRender, setInitialRender] = useState<boolean>(true);
   const videoRef = useRef<HTMLVideoElement>(null);
+
   useEffect(() => {
     if (videoRef.current && duration && initialRender) {
-      videoRef.current.currentTime = duration / 1000 + 5;
-    } else if (videoRef.current && videoRef.current.duration) {
-      videoRef.current.currentTime = 0;
-    }
-  }, [initialRender, videoRef.current?.duration]);
+      videoRef.current.currentTime = (duration + 500) / 1000;
+    } 
+  }, [initialRender]);
+
+  useEffect(() => {
+    videoRef.current?.addEventListener("durationchange", () => {
+      if (!Number.isNaN(videoRef.current?.duration)
+      && Number.isFinite(videoRef.current?.duration)) {
+        videoRef.current!.currentTime = 0;
+      }
+    })
+  }, []);
+  
+  
   return (<div className="video-item">
     <h6 className="video-title">{title}</h6>
     <video ref={videoRef} className="video-item-content" src={url} controls></video>
