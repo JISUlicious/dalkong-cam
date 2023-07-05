c64321c651044b902a705f29d44f5dd237550c91
JISUlicious <ljshong@hanmail.net>
2023-07-05 16:03:38 +0900

    wemb file duration workaround

--- src/common/components/VideoItem.tsx
+++ src/common/components/VideoItem.tsx
@@ -1,15 +1,25 @@
 import "../../common/styles/VideoItem.scss";
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface SavedVideoProps {
   title: string,
-  url: string
+  url: string,
+  duration?: number
 }
 
-export function VideoItem ({title, url}: SavedVideoProps) {
+export function VideoItem ({title, url, duration}: SavedVideoProps) {
+  const [initialRender, setInitialRender] = useState<boolean>(true);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    if (videoRef.current && duration && initialRender) {
+      videoRef.current.currentTime = duration / 1000 + 5;
+    } else if (videoRef.current && videoRef.current.duration) {
+      videoRef.current.currentTime = 0;
+    }
+  }, [initialRender, videoRef.current?.duration]);
   return (<div className="video-item">
     <h6 className="video-title">{title}</h6>
-    <video className="video-item-content" src={url} controls></video>
+    <video ref={videoRef} className="video-item-content" src={url} controls></video>
   </div>);
 }
