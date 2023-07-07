6c9fc6235b69252919f82695d95591ffaee6f91c
JISUlicious <ljshong@hanmail.net>
2023-07-07 23:40:22 +0900

    videolist card color fix

--- src/common/components/VideoItem.tsx
+++ src/common/components/VideoItem.tsx
@@ -6,8 +6,8 @@ interface SavedVideoProps {
 }
 
 export function VideoItem ({title, url}: SavedVideoProps) {
-  return (<div className="video-item card">
-    <div className="video-title card-body py-2">
+  return (<div className="video-item card border border-secondary">
+    <div className="video-title card-body py-2 bg-secondary">
       {title}
     </div>
     <video className="video-item-content card-img-bottom" src={url} controls></video>
--- src/common/components/VideosList.tsx
+++ src/common/components/VideosList.tsx
@@ -41,7 +41,7 @@ export function VideosList ({videos}: VideosListProps) {
       const second = datetime.getSeconds();
 
       const title = `${monthNames[month]} ${date} ${year} ${hour}:${minute < 10 ? `0${minute}` : minute}:${second < 10 ? `0${second}` : second}`;
-      return <div className="container px-2" key={data.deviceId + data.timestamp}>
+      return <div className="container-fluid px-2" key={data.deviceId + data.timestamp}>
         <VideoItem title={title} url={data.url} />
       </div>;
     })}
