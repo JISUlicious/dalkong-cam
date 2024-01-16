fd776e78784597e5e429d87029165c598899e998
JISUlicious <ljshong@hanmail.net>
2024-01-16 15:03:52 +0900

    feat: migration to firebase cli

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,58 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('@google-cloud/functions-framework');
+// The Firebase Admin SDK to access Firestore.
+const admin = require("firebase-admin");
+// const {deleteDoc, doc} = require("firebase-admin/firestore");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const db = admin.firestore();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Register a CloudEvent callback with the Functions Framework that will
+// be triggered by Cloud Storage.
+functions.cloudEvent('deleteDocOnRemovalOfStorageFile', (cloudEvent) => {
+  
+  console.log(`Event ID: ${cloudEvent.id}`);
+  console.log(`Event Type: ${cloudEvent.type}`);
+
+  const file = cloudEvent.data;
+  console.log(`Bucket: ${file.bucket}`);
+  console.log(`File: ${file.name}`);
+  console.log(`Metageneration: ${file.metageneration}`);
+  console.log(`Created: ${file.timeCreated}`);
+  console.log(`Updated: ${file.updated}`);
+  const fileName = file.name;
+  const userId = fileName.split('/')[1];
+
+  // db.collectionGroup('savedVideos')
+  //   .where('fullPath', '==', file.name)
+  //   .get()
+  //   .then((querySnapshot) => {
+  //     console.log('Query executed successfully');
+  //     querySnapshot.forEach(doc => {
+  //       console.log("target doc", doc.ref.path);
+  //       // deleteDoc(doc(db, doc.ref.path));
+  //       doc.ref.delete();
+  //     });
+  //   })
+  //   .catch((error) => {
+  //     console.error('Error executing query:', error);
+  //   });
+
+  db.collection(`users/${userId}/savedVideos`)
+    .where('fullPath', '==', file.name)
+    .get()
+    .then((querySnapshot) => {
+      console.log('Query executed successfully');
+      querySnapshot.forEach(doc => {
+        console.log("target doc", doc.ref.path);
+        doc.ref.delete();
+      });
+    })
+    .catch((error) => {
+      console.error('Error executing query:', error);
+    });
+
+});
+
